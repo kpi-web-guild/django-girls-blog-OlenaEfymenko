c3400e18753fd3a4d92ae93b39ddb6e3b39a5f2d
Olena <efymenko.olena@gmail.com>
2024-01-28 00:48:43 +0200

    Add a new path in the project URLconf
    The change provides including paths from the blog application urlpatterns.
    This allows adding future blog URLs without modifying the root URLconf
    file directly.

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,9 @@
 """mysite URL Configuration."""
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
 ]
