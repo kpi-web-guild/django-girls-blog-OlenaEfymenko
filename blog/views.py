bf5aff0e5506623ecc2ca327e74269e941018518
Olena <efymenko.olena@gmail.com>
2024-01-28 21:02:12 +0200

    Integrate the function to render a list of posts
    This patch provides a new URL and function for the Blog application.

--- blog/views.py
+++ blog/views.py
@@ -1,1 +1,16 @@
 """The views for the Blog application."""
+
+from django.shortcuts import render
+from django.utils import timezone
+
+from .models import Post
+
+
+def post_list(request):
+    """Display the posts ordered by published date."""
+    posts = (
+        Post.objects
+        .filter(published_date__lte=timezone.now())
+        .order_by('published_date')
+    )
+    return render(request, 'blog/post_list.html', {'posts': posts})
