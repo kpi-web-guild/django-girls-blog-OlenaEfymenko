5622865e399c19a2edd4ac2b2b8651ca2c4fb18a
Olena <efymenko.olena@gmail.com>
2024-03-28 00:32:28 +0200

    Integrate the function to implement the edition of the posts

--- blog/forms.py
+++ blog/forms.py
@@ -1,13 +1,15 @@
+"""Form class for the Blog application."""
+
 from django import forms
+
 from .models import Post
 
 
 class PostForm(forms.ModelForm):
     """Form for the creation of new posts in the Blog application."""
-    
+
     class Meta:
         """Metadata for the post form."""
+
         model = Post
-        fields = ('title', 'text',)
-        
-        
+        fields = ('title', 'text')
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,10 @@
 """The views for the Blog application."""
 
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.utils import timezone
-from django.shortcuts import redirect
 
-from .models import Post
 from .forms import PostForm
+from .models import Post
 
 
 def post_list(request):
@@ -37,4 +36,19 @@ def post_new(request):
     else:
         form = PostForm
     return render(request, 'blog/post_edit.html', {'form': form})
-    
+
+
+def post_edit(request, pk):
+    """Edit an existing post."""
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
