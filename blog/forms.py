d8c57c8c1aa2c5990c49cf9b8dc59f8140609f31
Olena <efymenko.olena@gmail.com>
2024-03-27 21:15:46 +0200

    Integrate the new functionality for the creation new posts

---
+++ blog/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Post
+
+
+class PostForm(forms.ModelForm):
+    """Form for the creation of new posts in the Blog application."""
+    
+    class Meta:
+        """Metadata for the post form."""
+        model = Post
+        fields = ('title', 'text',)
+        
+        
--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,10 @@
 
 from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
+from django.shortcuts import redirect
 
 from .models import Post
+from .forms import PostForm
 
 
 def post_list(request):
@@ -20,3 +22,19 @@ def post_detail(request, pk):
     """Display a blog post based on the post's primary key."""
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def post_new(request):
+    """Create a new post."""
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm
+    return render(request, 'blog/post_edit.html', {'form': form})
+    
