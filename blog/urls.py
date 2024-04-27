ac1f1e825874870e870bd9c9f595974d8c7c1059
Olena <107187316+OlenaYefymenko@users.noreply.github.com>
2024-04-27 23:31:17 +0300

    Update blog/urls.py
    Co-authored-by: Sviatoslav Sydorenko (Святослав Сидоренко) <wk.cvs.github@sydorenko.org.ua>

--- blog/urls.py
+++ blog/urls.py
@@ -11,5 +11,4 @@ urlpatterns = [
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
-
 ]
