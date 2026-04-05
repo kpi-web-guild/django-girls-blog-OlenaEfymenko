31bbeadc5bbf754ff08eae1a0fc4910db930566c
Olena <efymenko.olena@gmail.com>
2026-04-05 23:20:07 +0300

    Add form and view for creating new blog posts

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
 """The views for the Blog application."""
 
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.utils import timezone
 
 from .models import Post
+from .forms import PostForm
 
 
 def post_list(request):
@@ -20,3 +21,18 @@ def post_detail(request, pk):
     """Display a blog post based on the post's primary key."""
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def post_new(request):
+    """Display a form for creating a new blog post."""
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
